ee246e89d087ebcf79e2e50aa39fb4b35006498c
devMartinez3 <devmartinez@proton.me>
2026-02-23 09:37:30 -0500

    fix

--- prisma/generated/prisma/internal/class.ts
+++ prisma/generated/prisma/internal/class.ts
@@ -23,7 +23,7 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-      "value": "C:\\Projects\\pruebas-tecnicas\\fullstack-challenge bk\\back-challenge\\prisma\\generated\\prisma",
+      "value": "C:\\Projects\\pruebas-tecnicas\\repo-challenge\\fullstack-challenge-back\\prisma\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -37,7 +37,7 @@ const config: runtime.GetPrismaClientConfig = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Projects\\pruebas-tecnicas\\fullstack-challenge bk\\back-challenge\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Projects\\pruebas-tecnicas\\repo-challenge\\fullstack-challenge-back\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativePath": "../..",
